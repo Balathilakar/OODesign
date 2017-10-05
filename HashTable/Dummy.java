974e19effdcc67744c4e2a79ca1dbe4779aca71d
Balathilakar <bala.thilakar@gmail.com>
2017-10-04 22:10:38 -0500

    Dummy.java

---
+++ HashTable/Dummy.java
@@ -0,0 +1,20 @@
+package cracking.code.interviewQ.HashTable.OODesign;
+
+public class Dummy {
+	
+	String name;
+	int age;
+	
+	public int getAge(){
+		return age;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public String toString(){
+		return name+", "+age;
+	}
+
+}
